7d63a189dab4e5f7f5c3d8e5aa17540cec065c5b
avi <gajai.avinash@gmail.com>
2026-02-16 21:18:18 -0500

    2.4

--- src/components/Course.jsx
+++ src/components/Course.jsx
@@ -1,58 +1,69 @@
-const Header = ({name}) => {
-  console.log("Header Component Props");
-  console.log(name);
+const CourseList = ({course}) => {
   return (
-    <>
-      <h1>{name}</h1>
-    </>
-  );
-};
+    <div>
+  <Header course={course} />
+    
 
-const Part = (props) => {
+    </div>
+    
+  )
+}
+
+const Header = ({course}) => {
+  return (
+    <div>
+      {course.map((course, index) => (
+        <li key={index}>
+          <h1>{course.name}</h1>
+        </li>
+      ))}
+    </div>
+  )
+}
+
+const Part = ({props}) => {
   console.log("Part Component Props =");
   console.log(props.name, props.exercises);
   return (
-    <>
+    <div>
       <p>
         {props.name} {props.exercises}
       </p>
-    </>
-  );
-};
-
-const Content = ({parts}) => {
-  console.log("Content Component Props = ");
-  console.log(parts);
-  return (
-    <div>
-      {parts.map(part => <Part name={part.name} exercises ={part.exercises} />)}
     </div>
   );
 };
 
-const Total = ({parts}) => {
+const Content = ({ parts }) => {
+  console.log("Content Component Props = ");
+  console.log(parts);
+  return (
+    <div>
+      {parts.map((part) => (
+        <Part name={part.name} exercises={part.exercises} />
+      ))}
+    </div>
+  );
+};
+
+const Total = ({ parts }) => {
   console.log("Total Component Props =");
   console.log(parts);
-  const total = parts.reduce((acc,item)=> acc += item.exercises, 0 )
+  const total = parts.reduce((acc, item) => (acc += item.exercises), 0);
 
   return (
     <>
-      <div>
-        {" "}
-        Total Number of Exercises{" "}
-        {total}
-      </div>
+      <div> Total Number of Exercises {total}</div>
     </>
   );
 };
 
-const Course = (props) => {
-  
+const Course = ({courses}) => {
+  console.log("Course Component Props");
+  console.log(courses);
   return (
     <>
-      <Header name={props.course.name} />
-      <Content parts={props.course.parts} />
-      <Total parts={props.course.parts} />
+      <CourseList course={courses} />
+
     </>
   );
 };
